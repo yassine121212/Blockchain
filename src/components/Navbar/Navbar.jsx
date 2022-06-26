5f6d614b636c4b8d4e54260d92d6901b868e5a74
yassine121212 <elouazzaniyassine87@gmail.com>
2022-06-26 15:23:09 +0100

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="n-logo">
+
+        </div>
+        <div className="n-links">
+            <ul>
+                <li>WHITEPAPER</li>
+                <li>ROADMAP</li>
+                <li>AIRDROP</li>
+                <li>IDO EVENT</li>
+                <li>GAMEFI</li>
+                <li>DASHBOARD</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
