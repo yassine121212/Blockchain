ffe02a4daa8c9edaddcf828962c98d4ec91fadf6
yassine121212 <elouazzaniyassine87@gmail.com>
2022-06-27 01:44:47 +0100

    add items

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Main from '../../components/main/Main'
+import Cart from '../../components/Carts/Cart'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Main/>
+        <Cart/> 
     </div>
   )
 }
